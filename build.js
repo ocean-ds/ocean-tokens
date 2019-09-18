e72713545f79d29ca77d096c122d2960df79e66e
Elton <eltongarbin@gmail.com>
2019-09-18 20:41:27 -0300

    adding new tokens

--- build.js
+++ build.js
@@ -16,7 +16,8 @@ function getStyleDictionaryConfig(platform) {
         files: [
           {
             destination: 'tokens.js',
-            format: 'javascript/es6'
+            format: 'javascript/es6',
+            options: { showFileHeader: false }
           }
         ]
       },
@@ -34,7 +35,8 @@ function getStyleDictionaryConfig(platform) {
         files: [
           {
             destination: 'tokens.scss',
-            format: 'scss/variables'
+            format: 'scss/variables',
+            options: { showFileHeader: false }
           }
         ]
       },
@@ -51,24 +53,9 @@ function getStyleDictionaryConfig(platform) {
         files: [
           {
             destination: 'tokens.colors.xml',
-            format: 'android/colors'
+            format: 'android/colors',
+            options: { showFileHeader: false }
           }
-          // {
-          //   destination: 'tokens.dimens.xml',
-          //   format: 'android/dimens'
-          // },
-          // {
-          //   destination: 'tokens.font_dimens.xml',
-          //   format: 'android/fontDimens'
-          // },
-          // {
-          //   destination: 'tokens.integers.xml',
-          //   format: 'android/integers'
-          // },
-          // {
-          //   destination: 'tokens.strings.xml',
-          //   format: 'android/strings'
-          // }
         ]
       },
       ios: {
@@ -86,7 +73,8 @@ function getStyleDictionaryConfig(platform) {
         files: [
           {
             destination: 'tokens.h',
-            format: 'ios/macros'
+            format: 'ios/macros',
+            options: { showFileHeader: false }
           }
         ]
       },
@@ -106,7 +94,8 @@ function getStyleDictionaryConfig(platform) {
           {
             destination: 'tokens.swift',
             format: 'ios-swift/class.swift',
-            className: 'DesignTokens'
+            className: 'DesignTokens',
+            options: { showFileHeader: false }
           }
         ]
       }
--- dist/ios/tokens.swift
+++ dist/ios/tokens.swift
@@ -2,26 +2,21 @@
 //
 // tokens.swift
 //
-// Do not edit directly
-// Generated on Thu, 12 Sep 2019 12:48:31 GMT
+
 //
 
 
 import UIKit
 
 public class DesignTokens {
-    static let TOKEN_COLOR_BACKGROUND_BASE = UIColor(red: 0.17, green: 0.21, blue: 0.27, alpha:1)
-    static let TOKEN_COLOR_BACKGROUND_BUTTON_PRIMARY_ACTIVE = UIColor(red: 0.24, green: 0.69, blue: 0.75, alpha:1)
-    static let TOKEN_COLOR_BACKGROUND_BUTTON_PRIMARY_BASE = UIColor(red: 0.20, green: 0.77, blue: 0.84, alpha:1)
-    static let TOKEN_COLOR_BACKGROUND_BUTTON_SECONDARY_BASE = UIColor(red: 1.00, green: 1.00, blue: 1.00, alpha:1)
+    static let TOKEN_COLOR_BASE_BLACK_NORMAL = UIColor(red: 0.00, green: 0.00, blue: 0.00, alpha:1)
     static let TOKEN_COLOR_BASE_BLUE_DARK = UIColor(red: 0.17, green: 0.21, blue: 0.27, alpha:1)
-    static let TOKEN_COLOR_BASE_CYAN_MEDIUM = UIColor(red: 0.24, green: 0.69, blue: 0.75, alpha:1)
-    static let TOKEN_COLOR_BASE_CYAN_NORMAL = UIColor(red: 0.20, green: 0.77, blue: 0.84, alpha:1)
-    static let TOKEN_COLOR_BASE_GRAY_DARK = UIColor(red: 0.20, green: 0.24, blue: 0.31, alpha:1)
+    static let TOKEN_COLOR_BASE_BLUE_MEDIUM = UIColor(red: 0.54, green: 0.61, blue: 0.75, alpha:1)
+    static let TOKEN_COLOR_BASE_CYAN_NORMAL = UIColor(red: 0.00, green: 0.84, blue: 0.95, alpha:1)
+    static let TOKEN_COLOR_BASE_GRAY_LIGHT = UIColor(red: 0.94, green: 0.94, blue: 0.96, alpha:1)
+    static let TOKEN_COLOR_BASE_GRAY_NORMAL = UIColor(red: 0.35, green: 0.35, blue: 0.35, alpha:1)
+    static let TOKEN_COLOR_BASE_GREEN_NORMAL = UIColor(red: 0.00, green: 0.82, blue: 0.53, alpha:1)
+    static let TOKEN_COLOR_BASE_ORANGE_MEDIUM = UIColor(red: 1.00, green: 0.56, blue: 0.34, alpha:1)
+    static let TOKEN_COLOR_BASE_RED_NORMAL = UIColor(red: 0.93, green: 0.41, blue: 0.43, alpha:1)
     static let TOKEN_COLOR_BASE_WHITE = UIColor(red: 1.00, green: 1.00, blue: 1.00, alpha:1)
-    static let TOKEN_COLOR_FONT_BUTTON_PRIMARY_BASE = UIColor(red: 1.00, green: 1.00, blue: 1.00, alpha:1)
-    static let TOKEN_COLOR_FONT_BUTTON_SECONDARY_ACTIVE = UIColor(red: 0.20, green: 0.77, blue: 0.84, alpha:1)
-    static let TOKEN_COLOR_FONT_BUTTON_SECONDARY_BASE = UIColor(red: 0.20, green: 0.24, blue: 0.31, alpha:1)
-    static let TOKEN_FONT_FAMILY_BASE = "San Francisco"
-    static let TOKEN_FONT_PLATFORM_SYSTEM = "San Francisco"
 }
