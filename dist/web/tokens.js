b93db8e78a92f4c40fbd8daab9b5ca8f8e6d97bb
Elton <eltongarbin@gmail.com>
2019-09-24 22:30:23 -0300

    rename token file

--- dist/web/tokens.js
+++ dist/web/tokens.js
@@ -1,12 +1,11 @@
-export const TOKEN_COLOR_BASE_WHITE = '#ffffff';
-export const TOKEN_COLOR_BASE_BLACK_NORMAL = '#000000';
-export const TOKEN_COLOR_BASE_BLUE_MEDIUM = '#899bbe';
-export const TOKEN_COLOR_BASE_BLUE_DARK = '#2c3645';
-export const TOKEN_COLOR_BASE_CYAN_NORMAL = '#00d5f2';
-export const TOKEN_COLOR_BASE_GRAY_NORMAL = '#5a5a5a';
-export const TOKEN_COLOR_BASE_GRAY_LIGHT = '#efeff4';
-export const TOKEN_COLOR_BASE_RED_NORMAL = '#ec696d';
-export const TOKEN_COLOR_BASE_ORANGE_MEDIUM = '#ff9057';
-export const TOKEN_COLOR_BASE_GREEN_NORMAL = '#00d288';
-export const TOKEN_FONT_FAMILY_BASE =
-  "myriad-pro, 'Open Sans', 'Helvetica Neue', Helvetica, Arial, sans-serif";
+export const TOKEN_COLOR_BASE_WHITE = "#ffffff";
+export const TOKEN_COLOR_BASE_BLACK_NORMAL = "#000000";
+export const TOKEN_COLOR_BASE_BLUE_MEDIUM = "#899bbe";
+export const TOKEN_COLOR_BASE_BLUE_DARK = "#2c3645";
+export const TOKEN_COLOR_BASE_CYAN_NORMAL = "#00d5f2";
+export const TOKEN_COLOR_BASE_GRAY_NORMAL = "#5a5a5a";
+export const TOKEN_COLOR_BASE_GRAY_LIGHT = "#efeff4";
+export const TOKEN_COLOR_BASE_RED_NORMAL = "#ec696d";
+export const TOKEN_COLOR_BASE_ORANGE_MEDIUM = "#ff9057";
+export const TOKEN_COLOR_BASE_GREEN_NORMAL = "#00d288";
+export const TOKEN_FONT_FAMILY_BASE = "myriad-pro, 'Open Sans', 'Helvetica Neue', Helvetica, Arial, sans-serif";
