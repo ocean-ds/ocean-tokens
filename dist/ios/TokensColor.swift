478753b6c139f72e6a8518dc1ccc1bb0b4d37d51
Elton Garbin <eltongarbin@gmail.com>
2020-01-23 09:11:01 -0300

    changing the way of exporting tokens to improve team adherence (#7)

---
+++ dist/ios/TokensColor.swift
@@ -0,0 +1,21 @@
+
+//
+// TokensColor.swift
+//
+
+//
+
+import UIKit
+
+public enum TokensColor {
+    public static let baseBlackNormal = UIColor(red: 0.000, green: 0.000, blue: 0.000, alpha:1)
+    public static let baseBlueDark = UIColor(red: 0.173, green: 0.212, blue: 0.271, alpha:1)
+    public static let baseBlueMedium = UIColor(red: 0.537, green: 0.608, blue: 0.745, alpha:1)
+    public static let baseCyanNormal = UIColor(red: 0.000, green: 0.835, blue: 0.949, alpha:1)
+    public static let baseGrayLight = UIColor(red: 0.937, green: 0.937, blue: 0.957, alpha:1)
+    public static let baseGrayNormal = UIColor(red: 0.353, green: 0.353, blue: 0.353, alpha:1)
+    public static let baseGreenNormal = UIColor(red: 0.000, green: 0.824, blue: 0.533, alpha:1)
+    public static let baseOrangeMedium = UIColor(red: 1.000, green: 0.565, blue: 0.341, alpha:1)
+    public static let baseRedNormal = UIColor(red: 0.925, green: 0.412, blue: 0.427, alpha:1)
+    public static let baseWhite = UIColor(red: 1.000, green: 1.000, blue: 1.000, alpha:1)
+}
---
+++ dist/ios/TokensFont.swift
@@ -0,0 +1,12 @@
+
+//
+// TokensFont.swift
+//
+
+//
+
+import UIKit
+
+public enum TokensFont {
+    public static let familyBase = "San Francisco"
+}
