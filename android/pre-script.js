78613073f9e75fe5d7e35414f9fbd1d933489076
Blu <ops@useblu.com.br>
2020-06-18 16:57:16 +0000

    Updates

--- android/pre-script.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-
-const source = 'android/blu.android.xml.js';
-const target = 'node_modules/theo/lib/formats/blu.android.xml.js';
-
-fs.copyFile(source, target, (err) => {
-  if (err) throw err;
-  console.log(`${source} was copied to ${target}`);
-});
