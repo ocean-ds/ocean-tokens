d84ab61d6f1714d701c21a50db9af5848a4ccf5e
Luis Fernandez <luis.fernandez@mobiplus.com.br>
2020-06-25 11:53:28 -0300

    AFG-71 - Adjusting Android generation script

--- formats/android/pre-script.js
+++ formats/android/pre-script.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-const source = 'android/blu.android.xml.js';
+const source = 'formats/android/blu.android.xml.js';
 const target = 'node_modules/theo/lib/formats/blu.android.xml.js';
 
 fs.copyFile(source, target, (err) => {
