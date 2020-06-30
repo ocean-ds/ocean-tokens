0cf31b9b1f4cb7d34871a48c169356ba083e5841
alex gomes <alexandro.gomes@useblu.com.br>
2020-06-30 18:29:02 -0300

    generate token files by shell script 'generate_ios_tokens.sh'

--- generate_ios_tokens.sh
+++ generate_ios_tokens.sh
@@ -1,2 +1,7 @@
 echo "Generate tokens ios"
-npm run build:ios-typography && npm run build:ios-color && npm run build:ios-size && npm run build:ios
+#npm run build:ios-typography && npm run build:ios-color && npm run build:ios-size && npm run build:ios
+
+theo src/mobile-ios-color-tokens.yml --setup formats/ios/ios-formats.js --transform ios --format blu.ios.json --dest dist/ios --resolveMetaAliases &&
+theo src/mobile-ios-typography-tokens.yml --setup formats/ios/ios-formats.js --transform ios --format blu.ios.json --dest dist/ios --resolveMetaAliases &&
+theo src/mobile-ios-size-tokens.yml --setup formats/ios/ios-formats.js --transform ios --format blu.ios.json --dest dist/ios --resolveMetaAliases &&
+theo src/mobile-tokens.yml --setup formats/ios/ios-formats.js --transform ios --format blu.ios.json --dest dist/ios --resolveMetaAliases
