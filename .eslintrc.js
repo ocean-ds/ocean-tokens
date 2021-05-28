72552f10f0a6bcc969bf888d75f73c14b637b218
Elton Garbin <eltongarbin@gmail.com>
2021-05-28 17:47:47 -0300

    chore: bump dependencies (#139)

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,14 +4,13 @@ module.exports = {
     es2020: true,
     node: true,
   },
-  extends: ['standard', 'prettier', 'prettier/standard'],
-  plugins: ['import', 'prettier', 'standard'],
+  extends: ['standard', 'prettier'],
+  plugins: ['import', 'standard'],
   parserOptions: {
     ecmaVersion: 11,
   },
   rules: {
     'space-before-function-paren': 0,
     'new-cap': 0,
-    'prettier/prettier': 2,
   },
 };
