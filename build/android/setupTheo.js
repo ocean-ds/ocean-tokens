5af39c5c8a92624dddd3d771eb7efcd7c89cb07a
Luiz Thomaz <luiztajr@gmail.com>
2023-01-09 11:55:46 -0300

    chore: fix theo string concatenations

--- build/android/setupTheo.js
+++ build/android/setupTheo.js
@@ -5,26 +5,38 @@ module.exports = (theo) => {
     'android-font-size-sp',
     (prop) =>
       prop.get('type') === 'size' && prop.get('category') === 'font-size',
-    (prop) => theoReplaceAll(`${prop.get('value')}`, 'px', '') + 'sp'
+    (prop) => {
+      const replacedValue = theoReplaceAll(`${prop.get('value')}`, 'px', '');
+      return `${replacedValue}sp`;
+    }
   );
 
   theo.registerValueTransform(
     'android-border-width-dp',
     (prop) =>
       prop.get('type') === 'unit' && prop.get('category') === 'border-width',
-    (prop) => theoReplaceAll(`${prop.get('value')}`, 'px', '') + 'dp'
+    (prop) => {
+      const replacedValue = theoReplaceAll(`${prop.get('value')}`, 'px', '');
+      return `${replacedValue}dp`;
+    }
   );
 
   theo.registerValueTransform(
     'android-spacing-dp',
     (prop) => prop.get('type') === 'unit' && prop.get('category') === 'spacing',
-    (prop) => theoReplaceAll(`${prop.get('value')}`, 'px', '') + 'dp'
+    (prop) => {
+      const replacedValue = theoReplaceAll(`${prop.get('value')}`, 'px', '');
+      return `${replacedValue}dp`;
+    }
   );
 
   theo.registerValueTransform(
     'android-radius-dp',
     (prop) => prop.get('type') === 'unit' && prop.get('category') === 'radius',
-    (prop) => theoReplaceAll(`${prop.get('value')}`, 'px', '') + 'dp'
+    (prop) => {
+      const replacedValue = theoReplaceAll(`${prop.get('value')}`, 'px', '');
+      return `${replacedValue}dp`;
+    }
   );
 
   theo.registerTransform('android', [
